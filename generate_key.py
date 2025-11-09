8442bd7cb06a9cb16c10e2439fef5e456c70a0c9
Mayank  Mishra <MMishra@hitpa.co.in>
2025-11-09 21:54:06 +0530

    Initial commit: add UIIC app code

--- generate_key.py
+++
@@ -1,7 +0,0 @@
-from cryptography.fernet import Fernet
-
-key = Fernet.generate_key()
-with open("fernet_key.key", "wb") as f:
-    f.write(key)
-
-print("✅ Master Fernet key created as fernet_key.key")
